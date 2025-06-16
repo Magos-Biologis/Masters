e3c45f4de3f7557734a25723a4852f188c34de09
Magos-Biologis <127683931+Magos-Biologis@users.noreply.github.com>
2025-06-16 16:07:59 +0200

    Create operations.jl
    Adding vector calculus operations to the module

---
+++ Code/julia/Gillespies/src/vector_calculus/operations.jl
@@ -0,0 +1,16 @@
+
+"""
+Adding the divergence operation to exist under the '· * ·' binary operation
+"""
+function Base.:*(∇ :: Matrix{<: Differential}, A :: AbstractVector{<: Num})
+    return sum([xᵢ|> ∇ᵢ for (∇ᵢ, xᵢ) ∈ zip(∇, A)])
+end
+
+function Base.:*(∇ :: Matrix{<: Differential}, B :: AbstractMatrix{<: Num})
+    return collect([sum([xᵢ|> ∇ᵢ for (∇ᵢ, xᵢ) ∈ zip(∇,A)]) for A ∈ eachcol(B)])
+end
+
+# Base.:*(∇ :: Matrix{<: Differential}, A :: AbstractMatrix{<: Num}) = sum([xᵢ|> ∇ᵢ for (∇ᵢ, xᵢ) ∈ zip(∇,A)])
+
+# Base.:*(:: Val{:∇}, A :: Vector{<: Num}) = sum([xᵢ|> ∇ᵢ for (∇ᵢ, xᵢ) ∈ zip(∇,A)])
+# Base.:*(S :: Symbol, A :: Vector{<: Num}) = *(Val(S), A)
