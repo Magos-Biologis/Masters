7fb6447f524100795cb7dd27f822d0d1c741f815
Magos-Biologis <127683931+Magos-Biologis@users.noreply.github.com>
2025-06-19 12:06:38 +0200

    Update parameter_setters.jl
    I wanted to clean up some of the macro-expansion steps after having learned more about Julia macros.
    
    I lost a day on the fucking `@parameters` expanding in bizzare and confusing ways.
    Its barely an improvement now, but at least it's functional again.

--- Code/julia/Gillespies/src/vector_calculus/parameter_setters.jl
+++ Code/julia/Gillespies/src/vector_calculus/parameter_setters.jl
@@ -1,4 +1,15 @@
 
+
+
+
+
+
+# Base.convert(::Type{Num}, s::Symbol) = Symbolics.variable(s)
+# Base.convert(::Type{Num}, v::Vector{Symbol}) = eval( :( @parameters $(v...) ) )
+#
+#
+
+
 """
 A function to take the Dict of parameter assignments, and place it into a
 `@parameters` from ModelingToolkit
@@ -6,28 +17,29 @@ A function to take the Dict of parameter assignments, and place it into a
 Using some of the metaprograming features found in Julia, we basically construct
 the line of code that would say `@parameters x=1 y=2 ...` at parse time,
 so it can be compiled into the line of code it would otherwise be.
-
-Additionally we replace the existing dictionary that was fed into it.
 """
-macro parameterification(expr)
-    name = string(expr)
-    out  = quote dict_to_parameters($expr, $(Symbol(name))) end
-    return esc(:(eval($out)))
+macro parameterification(dictionary)
+
+    local dict = esc(dictionary)
+
+    local list₁ = :( [ :($k=$v) for (k,v) ∈  $dictionary ] )
+    local list₂ = :( variable.( keys($dict) ) .=> values($dict) )
+
+    local expr₁ = :( $parameter_expansion($list₁) )
+    local expr₂ = :( push!($dict, $list₂...) )
+
+    return quote $(esc(:(eval($expr₁)))); $expr₂ end
+
+end
+
+function parameter_expansion(exprs :: V) where V <: Vector{Expr}
+    expr = :( @parameters $(exprs...) )
+    return quote $expr end
 end
 
 
-"""
-The function called by the macro,
-I lost literal hours not noticing that I was missing a '...'
-"""
-function dict_to_parameters(D :: Dict{<: Any, <: Number}, name)
-    syms  = [ :($k=$v) for (k, v) ∈ D ]
-    expr₁ = quote @parameters $(syms...) end
 
-    new_dict = [Meta.parse(replace("$val", "=" => "=>")) for val ∈ syms]
-    expr₂ = quote new_dict = Dict(eval.($new_dict)) end
+# if !(Meta.isexpr(dictionary, :dict) | typeof(dictionary.args[1]) <: Dict)
+#     error("@parameterification must receive a literal Dict(...)")
+# end
 
-    expr₃ = quote push!($(name), new_dict...) end
-
-    return quote $expr₁; $expr₂; $expr₃ end
-end
